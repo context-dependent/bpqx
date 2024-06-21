8781e01e24d6861ac08f84d8c8ac2f66f38d9176
context-dependent <tomas.k.mcmanus@gmail.com>
2024-06-21 12:34:32 -0400

    pb fixes

--- R/fetch_responses.R
+++ R/fetch_responses.R
@@ -11,21 +11,32 @@ fetch_responses <- function(survey_id) {
         httr2::resp_body_json()
 
     pb <- progress::progress_bar$new(
-        format = glue::glue("fetch {survey_id} [:bar] :percent :eta"),
-        total = 100
+        format = "  fetching :what [:bar] (:spin) :percent",
+        total = 100,
+        show_after = 0.3
     )
-    progress <- 0
+    prog <- 0
     req_check <- req_base |>
         httr2::req_url_path_append(rsp_progress$result$progressId)
 
-    while (progress < 100) {
-        pb$tick()
+    while (prog < 100) {
         rsp_check <- req_check |>
             httr2::req_perform() |>
             httr2::resp_body_json()
+        
+        new_prog <- rsp_check$result$percentComplete
+        delta <- new_prog - prog
 
-        progress <- rsp_check$result$percentComplete
+        if(delta > 0) {
+            pb$update(new_prog / 100, tokens = list(what = survey_id))
+        } else {
+            pb$tick(tokens = list(what = survey_id))
+        }
+        
+        prog <- new_prog
     }
+
+    pb$terminate()
 
     tdir <- tempdir()
     tfile <- tempfile(tmpdir = tdir)
